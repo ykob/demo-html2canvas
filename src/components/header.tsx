8f79cf2fe4f9cbf2c0fb2ef8f3fdb6c36c1b5a0a
ykob <info@tplh.net>
2023-12-28 23:50:29 +0900

    added Header component.

---
+++ src/components/header.tsx
@@ -0,0 +1,21 @@
+import { ComponentProps } from "react";
+import { css, cx } from "styled-system/css";
+
+type HeaderProps = ComponentProps<"header">;
+
+export function Header({ className, ...props }: HeaderProps) {
+  return (
+    <header {...props} className={cx(styles.wrap, className)}>
+      <h1>html2canvas demo</h1>
+    </header>
+  );
+}
+
+const styles = {
+  wrap: css({
+    backgroundColor: "red",
+    color: "white",
+    padding: "1rem",
+    textAlign: "center",
+  }),
+};
